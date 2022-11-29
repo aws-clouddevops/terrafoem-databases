1ad6d7df0f1515009c501b3f945835ff9d087396
Sushma <sushma.gayatri451@gmail.com>
2022-11-29 22:21:03 +0530

    commit

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 module "mongodb" {
   source               = "./vendor/modules/mongodb"
   ENV                  = var.ENV
+}  
+
+module "redis" {
+  source               = "./vendor/modules/redis"
+  ENV                  = var.ENV
 }   
 
 
