910cd601b336caf0a979eb9ea3b88b9420396f5c
Sushma <sushma.gayatri451@gmail.com>
2022-12-04 10:37:52 +0530

    mysql

--- main.tf
+++ main.tf
@@ -6,7 +6,12 @@ module "mongodb" {
 module "redis" {
   source               = "./vendor/modules/redis"
   ENV                  = var.ENV
-}   
+}  
+
+module "mysql" {
+  source               = "./vendor/modules/mysql"
+  ENV                  = var.ENV
+}  
 
 
 # Always the source attribute in terraform module can not be parameterized
